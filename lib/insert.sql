7562b75d85c6fbb89a22b9d3c5d4005af17b5664
Matt Wright <mttwright@gmail.com>
2019-11-16 19:54:53 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 22, "Male", "blue", "happy", 1),
+(2, "Mr. Chocolate", 22, "Male", "blue", "happy", 1),
+(3, "Mr. Chocolate", 22, "Male", "blue", "happy", 1),
+(4, "Mr. Chocolate", 22, "Male", "blue", "happy", 1),
+(5, "Mr. Chocolate", 22, "Male", "blue", "happy", 1),
+(6, "Mr. Chocolate", 22, "Male", "blue", "happy", 1),
+(7, "Mr. Chocolate", 22, "Male", "blue", "happy", 1);
+
+INSERT INTO bears (id, age, gender, color, temperament, alive) VALUES
+(8, 22, "Ham", "Bologna", "Fiesta", 0);
